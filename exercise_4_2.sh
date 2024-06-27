63228f107e6dc480efa4f71693837733371bf03a
Deniz G <53919703+gokdumano@users.noreply.github.com>
2024-06-27 11:42:18 +0300

    Update exercise_4_2.sh

--- exercise_4_2.sh
+++ exercise_4_2.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Modify the script from the previous exercise. Make the "file_count"
+# function accept a directory as an argument. Next have the
+# function display the name of the directory followed by a colon.
+# Finally, display the number of files to the screen on the next
+# line. Call the function three times. First, on the "/etc"
+# directory, next on the "/var" directory and finally
+# on the "/usr/bin" directory.
+
 function file_count(){
 	local DIRPATH=$(readlink -f $1)
 	if [ -d $DIRPATH ]; then
