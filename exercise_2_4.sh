5564c7ee6b7315593177f2397d5c6cd08e4c14d3
Deniz G <53919703+gokdumano@users.noreply.github.com>
2024-06-27 11:37:15 +0300

    Update exercise_2_4.sh

--- exercise_2_4.sh
+++ exercise_2_4.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Write a shell script to check to see if the file "/etc/shadow" exists. If it does exist, display
+# "Shadow passwords are enabled." Next, check to see if you can write to the file. If you can,
+# display "You have permissions to edit /etc/shadow." If you cannot, display "You do NOT have
+# permissions to edit /etc/shadow."
+
 FPATH="/etc/shadow"
 
 if [ -f $FPATH ]
