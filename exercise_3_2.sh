01f4a7c461009f5dac54817db461a8ef7a353fec
Deniz G <53919703+gokdumano@users.noreply.github.com>
2024-06-27 11:39:31 +0300

    Update exercise_3_2.sh

--- exercise_3_2.sh
+++ exercise_3_2.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Write a shell script that accepts a file or directory name as an argument.
+# Have the script report if it is a regular file, a directory, or other type of
+# file. If it is a regular file, exit with a 0 exit status. If it is a directory,
+# exit with a 1 exit status. If it is some other type of file, exit with a 2 exit
+# status.
+
 FILE_NAME=$1
 
 if [ -z $FILE_NAME ]
