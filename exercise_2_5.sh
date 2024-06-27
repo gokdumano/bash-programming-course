c99db9b8a3356713680250683de331d332f9dffd
Deniz G <53919703+gokdumano@users.noreply.github.com>
2024-06-27 11:37:34 +0300

    Update exercise_2_5.sh

--- exercise_2_5.sh
+++ exercise_2_5.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Write a shell script that displays "man", "bear", "pig", "dog", "cat", and "sheep" to the screen with
+# each appearing on a separate line. Try to do this in as few lines as possible.
+# Hint: Loops can be used to perform repetitive tasks.
+
 ANIMALS="man bear pig dog cat sheep"
 
 for ANIMAL in $ANIMALS; do echo "$ANIMAL"; done
