77bd1a4985b6c94dff1d4dbf2503070d76afcc80
Deniz G <53919703+gokdumano@users.noreply.github.com>
2024-06-27 11:39:05 +0300

    Update exercise_3_1.sh

--- exercise_3_1.sh
+++ exercise_3_1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Write a shell script that displays "This script will exit with a 0 exit status." Be sure that the script does indeed exit with a 0 exit status.
+
 MESSAGE="This script will exit with a 0 exit status"
 
 echo $MESSAGE
