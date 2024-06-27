bdd7362b912ea332c5a645ab96e4f5a38b9f382d
Deniz G <53919703+gokdumano@users.noreply.github.com>
2024-06-27 11:36:29 +0300

    Update exercise_2_3.sh

--- exercise_2_3.sh
+++ exercise_2_3.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Store the output of the command "hostname" in a variable. Display "This script is running on
+# _______." where "_______" is the output of the "hostname" command.
+# Hint:
+# It's a best practice to use the ${VARIABLE} syntax if there is text or characters that directly
+# precede or follow the variable.
+
 MESSAGE="This script is running on $(hostname)."
 
 echo "$MESSAGE"
