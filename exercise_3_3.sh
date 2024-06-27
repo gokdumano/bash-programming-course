2f594128928968e0ce293a0880c54557a2b08f76
Deniz G <53919703+gokdumano@users.noreply.github.com>
2024-06-27 11:40:33 +0300

    Update exercise_3_3.sh

--- exercise_3_3.sh
+++ exercise_3_3.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Write a script that executes the command "cat /etc/shadow".
+# If the command returns a 0 exit status report "Command succeeded"
+# and exit with a 0 exit status. If the command returns a non­zero
+# exit status report "Command failed" and exit with a 1 exit status.
+
 cat /etc/shadow
 
 if [ 0 -eq $? ]; then
