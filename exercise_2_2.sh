73926f400be61916792a2058505390ec43528ede
Deniz G <53919703+gokdumano@users.noreply.github.com>
2024-06-27 11:35:57 +0300

    Update exercise_2_2.sh

--- exercise_2_2.sh
+++ exercise_2_2.sh
@@ -1,3 +1,6 @@
+# Modify the shell script from exercise_2_1 to include a variable. The variable will hold the contents
+# of the message "Shell Scripting is Fun!".
+
 #!/bin/bash
 
 MESSAGE="Shell Scripting is Fun!"
