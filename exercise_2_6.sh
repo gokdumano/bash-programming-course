9673d8f97eddc85d061b4d895664f799dc9d50d3
Deniz G <53919703+gokdumano@users.noreply.github.com>
2024-06-27 11:37:54 +0300

    Update exercise_2_6.sh

--- exercise_2_6.sh
+++ exercise_2_6.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Write a shell script that prompts the user for a name of a file or directory and reports if it is a
+# regular file, a directory, or other type of file.
+# Also perform an ls command against the file or directory with the long listing option.
+
 read -p "Enter the name of file/directory: " FNAME
 
 if [ -f $FNAME ]
